40eab22b9a68f06da688627a826f512d085bac0a
Slash <Slash.R.Wei@benQ.com>
2021-04-13 11:40:56 +0800

    init commit

--- ffmpeg/src/main/cpp/interface/native-ffmpeg-lib.cpp
+++ ffmpeg/src/main/cpp/interface/native-ffmpeg-lib.cpp
@@ -4,6 +4,9 @@ const char *getFFmpegVer() {
     return av_version_info();
 }
 
+/**
+ * 获取ffmpeg版本号
+ */
 extern "C"
 jstring Java_util_FfmpegUtil_getFfmpegVersion(JNIEnv *env, jobject thiz) {
     return env->NewStringUTF(getFFmpegVer());
